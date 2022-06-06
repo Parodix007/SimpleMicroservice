a89e66b41d725c82fc58e64a73c0391c93d94a7c
Parodix007 <sebastian.siarczynski@gmail.com>
2022-06-06 19:13:22 +0200

    init commit

---
+++ SimpleMicroservice/src/test/java/com/parodix/SimpleMicroservice/SimpleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parodix.SimpleMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
